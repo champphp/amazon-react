fcc15a08f1b4203b78f2f68dad944dba15da73fd
ChampPK <thechamp2535@gmail.com>
2020-12-04 00:04:43 +0700

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 
 import Header from './Header/Header'
+import Home from './Home/Home'
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <p>Page Checkout</p>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
